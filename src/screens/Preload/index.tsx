d5252efc4045886b92c3aa9cb2ed22d9c915ce1b
Luiz Pires <lpiresjunior@gmail.com>
2021-03-10 14:45:41 -0300

    Preload Screen & svg images

--- src/screens/Preload/index.tsx
+++ src/screens/Preload/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useEffect} from 'react';
+import {Container, LoadingIcon} from './styles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
+
+import BarberLogo from '../../assets/barber.svg';
 
 const Preload: React.FC = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    (async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+        // validar token
+      } else {
+        navigation.navigate('SignIn');
+      }
+    })();
+  }, [navigation]);
+
   return (
     <Container>
-      <Text>Preload</Text>
+      <BarberLogo width="100%" height="160" />
+      <LoadingIcon size="large" color="#FFF" />
     </Container>
   );
 };
