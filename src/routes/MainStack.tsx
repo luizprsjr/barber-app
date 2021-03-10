605b2a5d8f411167407286428a76a0014fda2825
Luiz Pires <lpiresjunior@gmail.com>
2021-03-10 14:23:40 -0300

    remove header from screens & set initialRouteName

--- src/routes/MainStack.tsx
+++ src/routes/MainStack.tsx
@@ -9,7 +9,11 @@ const Stack = createStackNavigator();
 
 const MainStack: React.FC = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      initialRouteName="Preload"
+      screenOptions={{
+        headerShown: false,
+      }}>
       <Stack.Screen name="Preload" component={Preload} />
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
