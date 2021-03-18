372947e6ca4268c98cbf4138841043211e2a63fa
Luiz Pires <lpiresjunior@gmail.com>
2021-03-18 11:15:44 -0300

    home styles

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useCallback, useState} from 'react';
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+} from './styles';
+
+import colors from '../../utils/colors';
+
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
+
+import {useNavigation} from '@react-navigation/native';
 
 const Home: React.FC = () => {
+  const navigation = useNavigation();
+
+  const [locationText, setLocationText] = useState('');
+
+  const goToSearch = useCallback(() => {
+    navigation.navigate('Search');
+  }, [navigation]);
+
   return (
     <Container>
-      <Text>Home</Text>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle numberOfLines={2}>
+            Encontre o seu barbeiro favorito
+          </HeaderTitle>
+          <SearchButton onPress={goToSearch}>
+            <SearchIcon width="26" height="26" fill={colors.white} />
+          </SearchButton>
+        </HeaderArea>
+
+        <LocationArea>
+          <LocationInput
+            placeholder="Onde você está"
+            placeholderTextColor={colors.white}
+            value={locationText}
+            onChangeText={(text) => setLocationText(text)}
+            selectionColor={colors.white}
+          />
+          <LocationFinder>
+            <MyLocationIcon width="24" height="24" fill={colors.white} />
+          </LocationFinder>
+        </LocationArea>
+      </Scroller>
     </Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,48 @@
 import styled from 'styled-components/native';
+import colors from '../../utils/colors';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${colors.blue};
+`;
+
+export const Scroller = styled.ScrollView`
+  flex: 1;
+  padding: 20px;
+`;
+
+export const HeaderArea = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const HeaderTitle = styled.Text`
+  width: 250px;
+  font-size: 24px;
+  font-weight: bold;
+  color: ${colors.white};
+`;
+export const SearchButton = styled.TouchableOpacity`
+  width: 26px;
+  height: 26px;
+`;
+
+export const LocationArea = styled.View`
+  flex-direction: row;
+  align-items: center;
+  background-color: ${colors.tabBlue};
+  height: 60px;
+  border-radius: 30px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-top: 30px;
+`;
+export const LocationInput = styled.TextInput`
+  flex: 1;
+  font-size: 16px;
+  color: ${colors.white};
+`;
+export const LocationFinder = styled.TouchableOpacity`
+  width: 24px;
+  height: 24px;
+`;
