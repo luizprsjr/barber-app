f8f40d9494ef2bae5fc10ca41e12b2d363a9cc5f
Luiz Pires <lpiresjunior@gmail.com>
2021-03-20 13:29:56 -0300

    logout

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useCallback} from 'react';
+import {Text, Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import api from '../../services/api';
+import {useNavigation} from '@react-navigation/native';
+
 import {Container} from './styles';
 
 const Profile: React.FC = () => {
+  const navigation = useNavigation();
+
+  const logout = useCallback(async () => {
+    const token = await AsyncStorage.getItem('token');
+
+    try {
+      await api.post('/auth/logout', {token});
+      navigation.navigate('SignIn');
+    } catch (error) {
+      console.log('ERROR: ', error);
+    }
+  }, [navigation]);
   return (
     <Container>
       <Text>Profile</Text>
+      <Button title="sair" onPress={logout} />
     </Container>
   );
 };
