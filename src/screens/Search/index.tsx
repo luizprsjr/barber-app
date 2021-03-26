4da19a40d08ed1984233a18e75ae80c6bb6b5ab6
Luiz Pires <lpiresjunior@gmail.com>
2021-03-26 03:49:18 -0300

    search screen

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,11 +1,82 @@
-import React from 'react';
-import {Text} from 'react-native';
-import {Container} from './styles';
+import React, {useCallback, useState} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Alert} from 'react-native';
+
+import api from '../../services/api';
+import BarberItem from '../../components/BarberItem';
+import IBarber from '../../interfaces/Barber';
+import colors from '../../utils/colors';
+
+import {
+  Container,
+  SearchArea,
+  SearchInput,
+  Scroller,
+  LoadingIcon,
+  ListArea,
+  EmptyWarning,
+} from './styles';
 
 const Search: React.FC = () => {
+  const [searchText, setSearchText] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [emptyList, setEmptyList] = useState(false);
+  const [list, setList] = useState<IBarber[]>([]);
+
+  const searchBarbers = useCallback(async () => {
+    setEmptyList(false);
+    setLoading(true);
+    setList([]);
+
+    if (searchText) {
+      const token = await AsyncStorage.getItem('token');
+      let {data: response} = await api.get(
+        `/search?q=${searchText}&token=${token}`,
+      );
+      if (!response.error) {
+        if (response.list.length > 0) {
+          setList(response.list);
+        } else {
+          setEmptyList(true);
+        }
+      } else {
+        Alert.alert('Erro!', `${response.error}`);
+      }
+    }
+
+    setLoading(false);
+  }, [searchText]);
+
   return (
     <Container>
-      <Text>Search</Text>
+      <SearchArea>
+        <SearchInput
+          placeholder="Digite o nome do barbeiro"
+          placeholderTextColor={colors.white}
+          value={searchText}
+          onChangeText={(t) => setSearchText(t)}
+          onEndEditing={searchBarbers}
+          returnKeyType="search"
+          autoFocus
+          selectTextOnFocus
+        />
+      </SearchArea>
+
+      <Scroller>
+        {loading && <LoadingIcon size="large" color={colors.white} />}
+
+        {emptyList && (
+          <EmptyWarning>
+            Não achamos barbeiros com o nome "{searchText}"
+          </EmptyWarning>
+        )}
+
+        <ListArea>
+          {list.map((item, k) => (
+            <BarberItem key={k} data={item} />
+          ))}
+        </ListArea>
+      </Scroller>
     </Container>
   );
 };
--- src/screens/Search/styles.ts
+++ src/screens/Search/styles.ts
@@ -1,3 +1,43 @@
 import styled from 'styled-components/native';
+import colors from '../../utils/colors';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  background-color: ${colors.blue};
+`;
+
+export const SearchArea = styled.View`
+  background-color: ${colors.tabBlue};
+  height: 40px;
+  border-radius: 20px;
+  padding: 0 20px;
+  margin: 20px;
+  margin-bottom: 0px;
+`;
+
+export const SearchInput = styled.TextInput`
+  flex: 1;
+  font-size: 16px;
+  color: ${colors.white};
+`;
+
+export const Scroller = styled.ScrollView`
+  flex: 1;
+  padding: 0 20px;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+  margin-top: 30px;
+`;
+
+export const ListArea = styled.View`
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+export const EmptyWarning = styled.Text`
+  text-align: center;
+  margin-top: 30px;
+  color: ${colors.white};
+  font-size: 14px;
+`;
